585d27af4e27d99e6c8fa068ca5c4e1b3e41fefa
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2019-05-20 09:51:38 +0300

    Update database.php

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 $server = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'kidsforum';
+$database = 'registration';
 
 try{
 	$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
